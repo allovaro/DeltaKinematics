98a1eda0348e897b7289759f60440f1902724eb3
Kirill Sokolov <Allovaro@gmail.com>
2018-11-12 16:18:41 +0300

    Commit

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from kinematicsinverse import Kinematics
 from detection import Detection
 from servocontrol import Servo
+import time
+
 
 svr = Servo()
-sd = Detection()
+#sd = Detection()
 ki = Kinematics()
 
-sd.vision()
+#sd.vision()
 svr.connect_servo()
-ki.x0 = 0
-ki.y0 = 0
-ki.z0 = -445.5
+# svr.read()
+ki.x0 = 250
+ki.y0 = 250
+ki.z0 = -450
 ki.delta_calc_inverse()
 svr.cmd(ki.theta1, ki.theta2, ki.theta3)
-
+# while True:
+#     ki.z0 -= 5
+#     ki.delta_calc_inverse()
+#     svr.cmd(ki.theta1, ki.theta2, ki.theta3)
+#     # svr.read()
+#     time.sleep(0.1)
 
--- servocontrol.py
+++ servocontrol.py
@@ -8,14 +8,14 @@ class Servo:
         self.usecMin = 600
         self.usecMax = 2200
         self.degMin = 20
-        self.degMax = 180
-        self.nullPoint = 1500
+        self.degMax = 110
+        self.nullPoint = 1400
 
     # Открытие COM1 порта для отправки углов
     def connect_servo(self):
         self.port = serial.Serial(
             port='COM1',
-            baudrate=9600,
+            baudrate=115200,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
@@ -25,15 +25,36 @@ class Servo:
     def cmd(self, theta1, theta2, theta3):
         buff = 's1;' + str(math.ceil(self.deg_to_usec(theta1))) + ':' + \
                's2;' + str(math.ceil(self.deg_to_usec(theta2))) + ':' + \
-               's3;' + str(math.ceil(self.deg_to_usec(theta3))) + '\r\n'
+               's3;' + str(math.ceil(self.deg_to_usec(theta3)))
         print(buff)
         self.port.write(buff.encode('ascii'))
         self.deg_to_usec(theta1)
 
     def deg_to_usec(self, theta):
-        if theta > 0:
-            return (self.usecMax - self.usecMin) * theta / self.degMax + self.nullPoint
         if theta < 0:
+            return (self.usecMax - self.usecMin) * theta * -1 / self.degMax + self.nullPoint
+        if theta > 0:
             return self.nullPoint - ((self.usecMax - self.usecMin) * theta / self.degMax)
         if theta == 0:
             return self.nullPoint
+
+    def read(self):
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+        cc = str(self.port.readline())
+        print(cc[2:][:-5])
+

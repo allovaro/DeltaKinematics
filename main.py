97f6373d4c12b472e71f87df57d184e8ad2e9bf7
Kirill Sokolov <Allovaro@gmail.com>
2018-11-22 11:34:34 +0300

    Commit

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ from kinematicsinverse import Kinematics
 from detection import Detection
 from servocontrol import Servo
 import time
-
+import math
 
 svr = Servo()
 sd = Detection()
 ki = Kinematics()
-frame = sd.countours2(sd.get_next_image())
-
-while True:
-    frame = sd.countours2(sd.get_next_image())
-    center = sd.detection_process(frame)
+# frame = sd.countours2(sd.get_next_image())
+#
+# while True:
+#     frame = sd.countours2(sd.get_next_image())
+#     center = sd.detection_process(frame)
 
 svr.connect_servo()
 # svr.read()
 # ki.x0 = 250
 # ki.y0 = 250
 # ki.z0 = -450
-ki.x0 = center[0]
-ki.y0 = center[1]
+ki.x0 = 10
+ki.y0 = 0
 ki.z0 = -443
+deg = 1
+r = 100
 print(ki.x0, ki.y0)
+
+# Движение по окружности с радиусом r
+while True:
+    ki.x0 = r * math.sin(math.radians(deg))
+    ki.y0 = r * math.sin(math.radians(90 - deg))
+    time.sleep(1)
+    print(ki.x0, ki.y0)
+    deg += 1
+
 ki.delta_calc_inverse()
 svr.cmd(ki.theta1, ki.theta2, ki.theta3)
 
